a3c90ad64a064cb6ee5a5215684235466abf3408
ruchika <bommidi.prem@gmail.com>
2023-03-28 19:19:43 +0200

    new script added

---
+++ getdogid.js
@@ -0,0 +1,28 @@
+import { sleep } from 'k6'
+import http from 'k6/http'
+
+export const options = {
+  scenarios: {
+    Scenario_1: {
+      executor: 'constant-vus',
+      gracefulStop: '30s',
+      duration: '5m',
+      vus: 1,
+      exec: 'scenario_1',
+    },
+  },
+}
+
+export function scenario_1() {
+  let response
+
+  // getdogid
+  response = http.get('https://petstore.swagger.io/v2/pet/1000', {
+    headers: {
+      Accept: 'application/json',
+    },
+  })
+
+  // Automatically added sleep
+  sleep(5)
+}
